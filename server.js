183189d64b37093ee9fb2a34518c8f7057aa9b07
Alexa <aleksa.for.learning@gmail.com>
2026-08-18 20:46:29 +0200

    Day 3: add JSON endpoint /api/info

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('Hello');
 });
 
+app.get("/api/info", (req, res) => {
+  res.json({
+    name: "Alexa",
+    project: "RAG assistant",
+    day: 3
+  });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
